980353bfa40ae1b47be4148e828c7e7a3c8deb89
Cody <cody.thompson22@gmail.com>
2012-11-21 04:51:10 -0700

    added a check in proc

--- procedures/checkout.sql
+++ procedures/checkout.sql
@@ -1,8 +1,25 @@
+set serveroutput on;
 create or replace procedure CheckOut(patronName IN varchar2, cId IN number)
 is
   dueDate date := sysdate + 14;
 begin
-  insert into loans
-  values(cId, GetPatronId(patronName), null, dueDate);
+  if not exists (select copyid from loans
+                 where copyid = cId and returned is null) then
+    insert into loans
+    values(cId, GetPatronId(patronName), null, dueDate);
+
+    dbms_output.put_line('Successfully Checked Out!');
+  else
+    dbms_output.put_line('That book is already checked out!');
+  end if;
+end;
+/
+
+create or replace procedure CheckIn(patronName IN varchar2, cId IN number)
+is
+  pId number := GetPatronId(patronName);
+begin
+  update loans set returned = sysdate
+  where copyid = cId and patronid = pId and returned is null;
 end;
 /
