82d5da664baa109bc736ac72dfa52d934ce29d0c
Cody Thompson <cdt27@nau.edu>
2012-11-20 20:42:31 -0700

    finished populate.sql for Copies table

--- populate.sql
+++ populate.sql
@@ -1,9 +1,16 @@
-insert into books
-values (9780863697654, 'Factotum', 'harles Bukowski', 'Black Sparrow Books',
-to_date('1975/01/01', 'yyyy/mm/dd'));
-insert into books
-values (9780140042528, 'The Dharma Bums', 'Jack Kerouac', 'The Viking Press',
-to_date('1958/01/01', 'yyyy/mm/dd')); 
-insert into books
-values (9780425043769, 'Dune', 'Frank Herbert', 'Chilton books', 
-to_date('1965/01/01', 'yyyy/mm/dd'));
+--insert into books
+--values (9780863697654, 'Factotum', 'harles Bukowski', 'Black Sparrow Books',
+--to_date('1975/01/01', 'yyyy/mm/dd'));
+--insert into books
+--values (9780140042528, 'The Dharma Bums', 'Jack Kerouac', 'The Viking Press',
+--to_date('1958/01/01', 'yyyy/mm/dd')); 
+--insert into books
+--values (9780425043769, 'Dune', 'Frank Herbert', 'Chilton books', 
+--to_date('1965/01/01', 'yyyy/mm/dd'));
+
+insert into Copies
+values (1, 9780863697654);
+insert into Copies
+values (2, 9780140042528);
+insert into Copies
+values (3, 9780425043769);
