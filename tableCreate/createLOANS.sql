4a32bec142e70e3ec34b19d7073218dadba9d3cc
codythompson <cdt27@nau.edu>
2012-11-20 18:07:59 -0700

    added a due date column to the loans table

--- tableCreate/createLOANS.sql
+++ tableCreate/createLOANS.sql
@@ -1,8 +1,9 @@
---drop table loans;
+drop table loans;
 create table loans(
 copyid number not null,
 constraint copyid_fk foreign key(copyid) references copies(copyid),
 patronid number not null,
 constraint patronid_fk foreign key(patronid) references patrons(patronid),
-returned date null
+returned date null,
+due date not null
 );
