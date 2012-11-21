ae2853627c27fc9716ae2fb821b99e63eada01e3
codythompson <cdt27@nau.edu>
2012-11-20 21:31:29 -0700

    ...

--- test/book.sql
+++ test/book.sql
@@ -1,4 +1,4 @@
-select copies.id, books.title loans.due
+select copies.copyid, books.title, loans.due
 from loans
 left join copies on loans.copyid = copies.copyid
 left join books on copies.isbn = books.isbn;
