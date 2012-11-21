cfe8b81be9e0d76aadf8a4207ddff2796c183a94
codythompson <cdt27@nau.edu>
2012-11-21 01:52:43 -0700

    check avail works

--- procedures/checkAvail.sql
+++ procedures/checkAvail.sql
@@ -2,9 +2,9 @@ set serveroutput on;
 create or replace procedure CheckAvailability (bookTitle IN varchar2)
 is
     numAvail number;
-    availDate varchar2;
+    availDate varchar2(10);
 begin
-    dbms_output.putline(bookTitle || ' availability:');
+    dbms_output.put_line(bookTitle || ' availability:');
 
     select count(c.copyid) into numAvail from copies c
     left join books on c.isbn = books.isbn
@@ -14,17 +14,17 @@ begin
         where loans.copyid = c.copyid and loans.returned is null);
 
     if numAvail > 0 then
-        dbms_output.putline(to_char(numAvail) || ' copies of ' || bookTitle ||
+        dbms_output.put_line(to_char(numAvail) || ' copies of ' || bookTitle ||
             ' available.');
     else
-        dbms_output.putline('No copies available.');
+        dbms_output.put_line('No copies available.');
 
         select to_char(min(loans.due), 'mm/dd/yyyy') into availDate from loans
         left join copies on loans.copyid = copies.copyid
         left join books on copies.isbn = books.isbn
         where lower(books.title) = lower(bookTitle);
 
-        dbms_output.putline('Expected available on: ' || availDate);
+        dbms_output.put_line('Expected available on: ' || availDate);
     end if;
 end;
 /
