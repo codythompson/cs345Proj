78bea530a23210bc333b24436124171ecdfdc20d
codythompson <cdt27@nau.edu>
2012-11-21 02:50:15 -0700

    cleaned up newpatron

--- procedures/newPatron.sql
+++ procedures/newPatron.sql
@@ -1,8 +1,7 @@
-CREATE OR REPLACE PROCEDURE NewPatron (patron_ID IN number, patronName IN varchar2, patronAddr IN varchar2, patronTelephone IN number)
-	
-IS
-	BEGIN
-		INSERT INTO Patrons (patronid, Name, Address, Telephone)
-		VALUES (patron_ID, patronName, patronAddr, patronTelephone);
-	END;
+CREATE OR REPLACE PROCEDURE NewPatron (patronName IN varchar2, patronAddr IN varchar2, patronTelephone IN number)
+IS 
+BEGIN
+  INSERT INTO Patrons (patronid, Name, Address, Telephone)
+  VALUES (patrons_next_pk(), patronName, patronAddr, patronTelephone);
+END;
 /
--- tableCreate/pkGENS.sql
+++ tableCreate/pkGENS.sql
@@ -4,6 +4,16 @@ is
   newPk number;
 begin
   select max(copyid) + 1 into newPk from copies;
-  return newPK;
+  return newPk;
+end;
+/
+
+create or replace function patrons_next_pk
+return number
+is
+  newPk number;
+begin
+  select max(patronid) + 1 into newPk from patrons;
+  return newPk;
 end;
 /
