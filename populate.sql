22c6294e8caaa5d668030a0ad4386de4a2aa7fc4
Cody Thompson <cdt27@nau.edu>
2012-11-20 20:38:05 -0700

    finished populate.sql for books table

--- populate.sql
+++ populate.sql
@@ -1,8 +1,9 @@
 insert into books
-values (9780863697654, Factotum, harles Bukowski, Black Sparrow Books,
+values (9780863697654, 'Factotum', 'harles Bukowski', 'Black Sparrow Books',
 to_date('1975/01/01', 'yyyy/mm/dd'));
 insert into books
-values (9780140042528, The Dharma Bums, Jack Kerouac, The Viking Press,
+values (9780140042528, 'The Dharma Bums', 'Jack Kerouac', 'The Viking Press',
 to_date('1958/01/01', 'yyyy/mm/dd')); 
 insert into books
-values (
+values (9780425043769, 'Dune', 'Frank Herbert', 'Chilton books', 
+to_date('1965/01/01', 'yyyy/mm/dd'));
