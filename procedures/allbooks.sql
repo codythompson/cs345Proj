75760ee75c990367ba5e634dbe054d2981bcf329
codythompson <cdt27@nau.edu>
2012-11-21 05:37:35 -0700

    done with allbooks

--- procedures/allbooks.sql
+++ procedures/allbooks.sql
@@ -1,11 +1,11 @@
 create or replace view all_out
 as
 select copies.copyid copyid, books.isbn isbn, books.title title,
-  patrons.patronid loanerid, patrons.name loanername, loans.due due, 
-  (loans.due < sysdate) overdue
+  patrons.patronid loanerid, patrons.name loanername, loans.due due--, 
+  --(loans.due < sysdate) overdue
 from loans 
 left join copies on loans.copyid  = copies.copyid
 left join books on copies.isbn = books.isbn
 left join patrons on loans.patronid = patrons.patronid
-where loans.due is null
+where loans.returned is null
 order by books.isbn, copies.copyid;
