ab7e6f8d1019317ff86f366c94a697d16bfcd201
Cody <cody.thompson22@gmail.com>
2012-11-21 03:34:34 -0700

    added loanHist

---
+++ procedures/loanHist.sql
@@ -0,0 +1,41 @@
+set serveroutput on;
+
+create or replace function GetPatronId (patronName varchar2)
+return number
+is
+  pId number;
+begin
+  select patronid into pId from patrons
+  where lower(name) = lower(patronName);
+end;
+/
+
+create or replace procedure LoanHistory (patronName IN varchar2)
+is
+  cursor loans_cur is
+  select book.title title, loans.due due, loans.returned returned
+  from loans
+  left join copies on loans.copyid = copies.copyid
+  left join books on copies.isbn = books.isbn
+  where loans.patronid = GetPatronId()
+  order by loans.returned nulls first, loans.due;
+begin
+  dbms_output.put_line(patronName || ' Loan History:');
+
+  for rec in loans_cur
+  loop
+    dbms_output.put_line('----------------------');
+    dbms_output.put_line(rec.title);
+    dbms_output.put_line('DUE     : ' || to_char(rec.due, 'mm/dd/yyyy'));
+    if rec.returned is null then
+      dbms_output.put_line('RETURNED: STILL OUT!');
+    else
+      dbms_output.put_line('RETURNED: ' || to_char(rec.returned, 'mm/dd/yyyy'));
+    end if;
+  end loop;
+  
+  dbms_output.put_line();
+  dbms_output.put_line('END OF REPORT');
+  dbms_output.put_line();
+end;
+/
