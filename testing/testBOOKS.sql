24d3644b50e089854a5fcfcefbfc0b7623ee7b9c
Cody Thompson <cody.thompson22@gmail.com>
2012-12-06 16:50:54 -0700

    Added testing script for the BOOKS table

---
+++ testing/testBOOKS.sql
@@ -0,0 +1,34 @@
+-- This should insert just fine
+insert into books
+(isbn, title, author, publisher, publishdate)
+values (654321, "TEST BOOK", "Test Author", "Some Publisher", SYSDATE);
+
+-- This should NOT insert because the PK is the same
+insert into books
+(isbn, title, author, publisher, publishdate)
+values (654321, "TEST BOOK", "Test Author", "Some Publisher", SYSDATE);
+
+-- This should NOT insert because isbn is null
+insert into books
+(isbn, title, author, publisher, publishdate)
+values (NULL, "TEST BOOK", "Test Author", "Some Publisher", SYSDATE);
+
+-- This should NOT insert because author is null
+insert into books
+(isbn, title, author, publisher, publishdate)
+values (654322, NULL, "Test Author", "Some Publisher", SYSDATE);
+
+-- This should NOT insert because title is null
+insert into books
+(isbn, title, author, publisher, publishdate)
+values (654322, "TEST BOOK", NULL, "Some Publisher", SYSDATE);
+
+-- This should NOT insert because publisher is null
+insert into books
+(isbn, title, author, publisher, publishdate)
+values (654322, "TEST BOOK", "TEST TITLE", NULL, SYSDATE);
+
+-- This should NOT insert because publishdate is null
+insert into books
+(isbn, title, author, publisher, publishdate)
+values (654322, "TEST BOOK", "TEST TITLE", "Some Publisher", NULL);
