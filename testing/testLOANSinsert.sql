3a7b3add59b219a37bd5bad48f0b6f0896619a69
Cody <cody.thompson22@gmail.com>
2012-12-06 21:20:15 -0700

    more fun with PKs

--- testing/testLOANSinsert.sql
+++ testing/testLOANSinsert.sql
@@ -1,7 +1,7 @@
 -- Here is a working test of a loans entry
 insert into cdt27.loans
 (copyid, patronid, returned, due)
-values (7, 8, null, SYSDATE);
+values (6, 7, null, SYSDATE);
 
 -- Here is a NON working test of a loans entry (no null values except for due)
 insert into cdt27.loans
@@ -12,12 +12,12 @@ values (null, null, null, null);
 
 insert into cdt27.loans
 (copyid, patronid, returned, due)
-values (8, 9, SYSDATE, SYSDATE);
+values (7, 7, SYSDATE, SYSDATE);
 
 -- Here the insert does NOT work because it has a improper patronid_fk referenced
 
 insert into cdt27.loans
 (copyid, patronid, returned, due)
-values (7, 9, SYSDATE, SYSDATE);
+values (6, 8, SYSDATE, SYSDATE);
 
 rollback;
