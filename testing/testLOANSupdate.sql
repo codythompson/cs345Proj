e1c66293a981cff380b466a3e5faaa3ac24d00ae
Cody Thompson <cdt27@nau.edu>
2012-12-07 01:13:50 -0700

    added delete test for loans

--- testing/testLOANSupdate.sql
+++ testing/testLOANSupdate.sql
@@ -1,16 +1,16 @@
 -- this should update just fine where patronid_fk and copid_fk exist in patron and copies
-update loans
+update cdt27.loans
 set copyid = 5, patronid = 1, returned = SYSDATE, due = SYSDATE
 where patronid = 6 AND copyid = 2;
 
--- this should fail because none of the columns in the loans table allow nulls
-update loans
+-- this should fail because none of the columns in the cdt27.loans table allow nulls
+update cdt27.loans
 set copyid = null, patronid = null, due = null
 where patronid = 1 AND copyid = 5;
 
 -- because the two FK are referenced from other tables and if the data doesn't exist the update 
 -- will fail
-update loans
+update cdt27.loans
 set patronid = 1111, copyid = 2222
 where patronid = 1 AND copyid = 5;
 
