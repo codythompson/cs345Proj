b542ceda04862a3acda860e61a6e477f8bfaf7aa
Cody Thompson <cdt27@nau.edu>
2012-12-07 00:16:04 -0700

    tweak

--- testing/testADMINupdate.sql
+++ testing/testADMINupdate.sql
@@ -6,12 +6,12 @@ where adminid = 2;
 -- this should fail because none of the columns in the admin table allow nulls
 update admin
 set adminid = null, name = null, address = null, telephone = null
-where adminid = 2;
+where adminid = 1;
 
 -- this should fail because it changes the adminid_pk of an entry to an
 -- existing pk
 update admin
-set adminid = 1 
-where adminid = 2;
+set adminid = 99
+where adminid = 1;
 
 rollback;
