430ee4fc47715e4514a8761324d3edbbc451fcb6
Cody Thompson <cdt27@nau.edu>
2012-12-07 00:10:16 -0700

    Added Mark's test script for updating admin

---
+++ testing/testADMINupdate.sql
@@ -0,0 +1,17 @@
+-- this should update just fine
+update admin
+set adminid = 99, name = 'cody', address = '15 awesome st.', telephone = 6665554444, superuser = 0
+where adminid = 2;
+
+-- this should fail because none of the columns in the admin table allow nulls
+update admin
+set adminid = null, name = null, address = null, telephone = null
+where adminid = 2;
+
+-- this should fail because it changes the adminid_pk of an entry to an
+-- existing pk
+update admin
+set adminid_pk = 1 
+where adminid_pk = 2;
+
+rollback;
