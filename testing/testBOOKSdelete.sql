bd17c308e0fffe2c7c050945122ac8c4c8fdd7d1
Cody Thompson <cdt27@nau.edu>
2012-12-07 00:42:50 -0700

    added cdt27 qualifier to books test script

--- testing/testBOOKSdelete.sql
+++ testing/testBOOKSdelete.sql
@@ -1,13 +1,13 @@
 -- this insertion is to test deleting an entry with no foreign keys references
-insert into books
+insert into cdt27.books
 values (99, 'test', 'test', 'test', SYSDATE);
 
 -- this should work
-delete from books
+delete from cdt27.books
 where isbn = 99;
 
 -- this should violate a fk constraint on the copies table
-delete from books
+delete from cdt27.books
 where isbn = 9780140042528;
 
 rollback;
--- testing/testBOOKSupdate.sql
+++ testing/testBOOKSupdate.sql
@@ -1,16 +1,16 @@
 -- this should update just fine
-update books
+update cdt27.books
 set title = 'blah', author = 'blah', publisher = 'blah', publishdate = SYSDATE
 where isbn = 9780140042528;
 
--- this should fail because none of the columns in the books table allow nulls
-update books
+-- this should fail because none of the columns in the cdt27.books table allow nulls
+update cdt27.books
 set isbn = NULL, title = NULL, author = NULL, publisher = NULL, publishdate = NULL
 where isbn = 9780140042528;
 
 -- this should fails because it changes the isbn of an entry that other rows
 -- reference
-update books
+update cdt27.books
 set isbn = 111
 where isbn = 9780140042528;
 
