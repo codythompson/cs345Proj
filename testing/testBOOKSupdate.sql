e1ccf4134ace46d880fdc69e9d71fac94a816b1c
codythompson <cdt27@nau.edu>
2012-12-06 18:30:07 -0700

    ...

--- testing/testBOOKSupdate.sql
+++ testing/testBOOKSupdate.sql
@@ -1,9 +1,11 @@
 -- this should update just fine
 update books
-set title = "blah", author = "blah", publisher = "blah", publishdate = SYSDATE
-where isbn = 9780140042528
+set title = 'blah', author = 'blah', publisher = 'blah', publishdate = SYSDATE
+where isbn = 9780140042528;
 
 -- this should fail because none of the columns in the books table allow nulls
 update books
 set isbn = NULL, title = NULL, author = NULL, publisher = NULL, publishdate = NULL
-where isbn = 9780140042528
+where isbn = 9780140042528;
+
+rollback;
