6f11df997411109daf6cbbb802d6349bc5105884
Cody Thompson <cdt27@nau.edu>
2012-12-07 00:44:45 -0700

    qaul for copies update

--- testing/testCOPIESupdate.sql
+++ testing/testCOPIESupdate.sql
@@ -1,15 +1,15 @@
 -- this should update just fine (because this ISBN exists)
-update copies
+update cdt27.copies
 set isbn = 9780140042528
 where copyid = 6;
 
--- this should fail because none of the columns in the copies table allow nulls
-update copies
+-- this should fail because none of the columns in the cdt27.copies table allow nulls
+update cdt27.copies
 set isbn = null, copyid = null
 where copyid = 6;
 
 -- this should fail because it changes the isbn of an entry that is referenced by books(isbn) (it doesn't exist so the reference breaks)
-update copies
+update cdt27.copies
 set isbn = 8888888888
 where copyid = 6;
 
