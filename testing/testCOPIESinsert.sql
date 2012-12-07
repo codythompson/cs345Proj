f0135821535f0c2ff52d24ec707b4b26d8086f4d
Cody <cody.thompson22@gmail.com>
2012-12-06 20:30:40 -0700

    added mark's testCOPIESinsert.sql

---
+++ testing/testCOPIESinsert.sql
@@ -0,0 +1,31 @@
+-- This should NOT insert because this table has no column that allows nulls
+insert into cdt27.copies
+(copyid, isbn)
+values (null, null);
+
+-- This should insert just fine
+insert into cdt27.copies
+(copyid, isbn)
+values (2, 1534);
+
+-- This should NOt insert because the PK is the same as above
+insert into cdt27.copies
+(copyid, isbn)
+values (2, 1535);
+
+-- Here is a working test of a books entry
+insert into cdt27.books
+(isbn, title, author, publisher, publishdate)
+values (654321, 'TEST BOOK', 'Test Author', 'Some Publisher', SYSDATE);
+
+-- Here the insert does work because it has a proper ISBN referenced
+insert into cdt27.copies
+(copyid, isbn)
+values (2, 654321);
+
+-- Here the insert does NOT work because it has a improper ISBN referenced
+insert into cdt27.copies
+(copyid, isbn)
+values (2, 44444);
+
+rollback;
