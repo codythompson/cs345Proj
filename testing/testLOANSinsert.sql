1acff55495f87c44d5edbd61f6f5e11072c24aa5
Cody <cody.thompson22@gmail.com>
2012-12-06 21:16:14 -0700

    added semicolons

--- testing/testLOANSinsert.sql
+++ testing/testLOANSinsert.sql
@@ -1,22 +1,22 @@
 -- Here is a working test of a loans entry
-insert into cd27.loans
+insert into cdt27.loans
 (copyid, patronid, returned, due)
 values (7, 8, SYSDATE, null);
 
 -- Here is a NON working test of a loans entry (no null values except for due)
-insert into cd27.loans
+insert into cdt27.loans
 (copyid, patronid, returned, due)
 values (null, null, null, null);
 
 -- Here the insert does NOT work because it has a improper copyid_fk referenced
 
-insert into cd27.loans
+insert into cdt27.loans
 (copyid, patronid, returned, due)
 values (8, 9, SYSDATE, SYSDATE);
 
 -- Here the insert does NOT work because it has a improper patronid_fk referenced
 
-insert into cd27.loans
+insert into cdt27.loans
 (copyid, patronid, returned, due)
 values (7, 9, SYSDATE, SYSDATE);
 
