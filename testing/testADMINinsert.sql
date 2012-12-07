ed68e2ffd0f1db20f4a374046ba58e4a0a8f5735
Cody <cody.thompson22@gmail.com>
2012-12-06 20:19:14 -0700

    slight changes to the testADMINinsert.sql script

--- testing/testADMINinsert.sql
+++ testing/testADMINinsert.sql
@@ -1,14 +1,14 @@
 -- This should NOT insert because this table has no column that allows nulls
 insert into cdt27.admin
-(adminid, adminid_pk, name, address, telephone, superuser)
-values (null, null, null, null, null, null);
+(adminid, name, address, telephone, superuser)
+values (null, null, null, null, null);
 
 -- This should insert just fine
 insert into cdt27.admin
-(adminid, adminid_pk, name, address, telephone, superuser)
-values (1, 1234, 'Mark', '5 Street', 1234567, 0);
+(adminid, name, address, telephone, superuser)
+values (234, 'Mark', '5 Street', 1234567, 0);
 
 -- This should NOT insert because the PK is the same
 insert into cdt27.admin
-(adminid, adminid_pk, name, address, telephone, superuser)
-values (2, 1234, 'Cody', '6 Street', 1234568, 1);
+(adminid, name, address, telephone, superuser)
+values (234, 'Cody', '6 Street', 1234568, 1);
