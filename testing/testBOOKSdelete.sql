e5385789dd3fd09fe1cca5141ad2986b1669d15c
Cody Thompson <cdt27@nau.edu>
2012-12-07 00:30:33 -0700

    Added script for testing deleting from books table

--- testing/testBOOKSdelete.sql
+++ testing/testBOOKSdelete.sql
@@ -1,2 +1,13 @@
 -- this insertion is to test deleting an entry with no foreign keys references
-insert into 
+insert into books
+values (99, "test", "test", "test", SYSDATE);
+
+-- this should work
+delete from books
+where isbn = 99;
+
+-- this should violate a fk constraint on the copies table
+delete from books
+where isbn = 9780140042528;
+
+rollback;
