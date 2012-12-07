661877f995eec0c92dad32a95be80ab2229676ea
Cody Thompson <cdt27@nau.edu>
2012-12-07 01:15:32 -0700

    woops

--- testing/testPATRONSdelete.sql
+++ testing/testPATRONSdelete.sql
@@ -1,5 +1,13 @@
+-- this insertion is to test deleting an entry with no foreign keys references
+insert into cdt27.patrons
+values (8, 'blah', 'blah', 1111111);
+
 -- this should work
-delete from cdt27.loans
-where copyid = 3 AND patronid = 1;
+delete from cdt27.patrons
+where patronid = 8;
+
+-- this should violate a fk constraint on the loans table
+delete from cdt27.patrons
+where patronid = 1;
 
 rollback;
