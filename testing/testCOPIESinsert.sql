d06f6ac58d8b919a5fe43176ced1346ad51d8b2e
Cody <cody.thompson22@gmail.com>
2012-12-06 20:51:07 -0700

    another PK issue fixed

--- testing/testCOPIESinsert.sql
+++ testing/testCOPIESinsert.sql
@@ -26,6 +26,6 @@ values (8, 654321);
 -- Here the insert does NOT work because it has a improper ISBN referenced
 insert into cdt27.copies
 (copyid, isbn)
-values (8, 44444);
+values (9, 44444);
 
 rollback;
